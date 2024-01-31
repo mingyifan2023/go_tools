5344220b7768315aee9ea7301365bb3c07d58d25
mingyifan2023 <150598503+mingyifan2023@users.noreply.github.com>
2024-01-31 15:17:31 +0900

    Update download_img.go

--- 1.下载图片/download_img.go
+++ 1.下载图片/download_img.go
@@ -10,22 +10,23 @@ func main() {
 	url := "https://api.multiavatar.com/653aa6243a36b92bf4ce2fd2eab3e348.png"
 	response, err := http.Get(url)
 	if err != nil {
-		fmt.Println("HTTP GET 请求失败:", err)
-		return
+		return fmt.Errorf("HTTP GET 请求失败:", err)
+		
 	}
 	defer response.Body.Close()
 
 	data, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		fmt.Println("读取响应数据失败:", err)
-		return
+		return fmt.Errorf("读取响应数据失败:", err)
+		
 	}
 
 	err = ioutil.WriteFile("/home/653aa6243a36b92bf4ce2fd2eab3e348.png", data, 0644)
 	if err != nil {
-		fmt.Println("保存文件失败:", err)
-		return
+		return  fmt.Errorf("保存文件失败:", err)
+		
 	}
 
 	fmt.Println("图片已成功下载并保存")
+	return nil 
 }
