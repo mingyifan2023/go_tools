cc82c5d4bae13ef2a481c235f9ad577cf19e5ced
mingyifan2023 <150598503+mingyifan2023@users.noreply.github.com>
2024-08-31 13:07:34 +0900

    Update main.go

--- 5.todolist/main.go
+++ 5.todolist/main.go
@@ -21,7 +21,6 @@ type Todo struct {
 // 初始化数据库
 func initDB() (*gorm.DB, error) {
 	dbDir := "DB"
-	// 创建 DB 文件夹（如果不存在）
 	if _, err := os.Stat(dbDir); os.IsNotExist(err) {
 		os.Mkdir(dbDir, os.ModePerm)
 	}
@@ -32,7 +31,6 @@ func initDB() (*gorm.DB, error) {
 		return nil, err
 	}
 
-	// 自动迁移模式
 	err = db.AutoMigrate(&Todo{})
 	if err != nil {
 		return nil, err
@@ -41,7 +39,28 @@ func initDB() (*gorm.DB, error) {
 	return db, nil
 }
 
-// 保存待办事项到数据库
+// 查询待办事项
+func getTodosHandler(w http.ResponseWriter, r *http.Request) {
+	db, err := initDB()
+	if err != nil {
+		http.Error(w, "数据库错误", http.StatusInternalServerError)
+		log.Fatal(err)
+		return
+	}
+
+	var todos []Todo
+	result := db.Find(&todos)
+	if result.Error != nil {
+		http.Error(w, "查询数据失败", http.StatusInternalServerError)
+		log.Fatal(result.Error)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todos)
+}
+
+// 保存或更新待办事项到数据库
 func saveTodoHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "不支持的方法", http.StatusMethodNotAllowed)
@@ -61,19 +80,24 @@ func saveTodoHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 		return
 	}
-	defer func() {
-		db.Exec("PRAGMA foreign_keys = ON") // 确保启用外键支持
-	}()
 
-	// 插入数据
-	result := db.Create(&todo)
-	if result.Error != nil {
-		http.Error(w, "插入数据失败", http.StatusInternalServerError)
-		log.Fatal(result.Error)
-		return
+	// 检查 ID 是否存在以决定是插入新记录还是更新现有记录
+	if todo.ID == 0 {
+		result := db.Create(&todo)
+		if result.Error != nil {
+			http.Error(w, "插入数据失败", http.StatusInternalServerError)
+			log.Fatal(result.Error)
+			return
+		}
+	} else {
+		result := db.Save(&todo) // 更新现有待办事项
+		if result.Error != nil {
+			http.Error(w, "更新数据失败", http.StatusInternalServerError)
+			log.Fatal(result.Error)
+			return
+		}
 	}
 
-	// 返回成功响应
 	response := map[string]interface{}{
 		"data": todo,
 	}
@@ -84,16 +108,6 @@ func saveTodoHandler(w http.ResponseWriter, r *http.Request) {
 
 // 显示 HTML 模板
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-
-	db, err := initDB()
-	if err != nil {
-		http.Error(w, "数据库错误", http.StatusInternalServerError)
-		log.Fatal(err)
-		return
-	}
-	defer func() {
-		db.Exec("PRAGMA foreign_keys = ON") // 确保启用外键支持
-	}()
 	tmpl, err := template.ParseFiles("./templates/index.html")
 	if err != nil {
 		http.Error(w, "无法加载模板", http.StatusInternalServerError)
@@ -104,10 +118,9 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// 初始化路由
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/save", saveTodoHandler)
+	http.HandleFunc("/todos", getTodosHandler) // 获取待办事项的路由
 
 	fmt.Println("服务器正在运行，访问地址: http://localhost:8004")
 	log.Fatal(http.ListenAndServe(":8004", nil))
-}
